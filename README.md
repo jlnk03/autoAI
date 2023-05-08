b3b9e09c8f3f1fbee02b32324b0b6c7fcd6be855
jlnk03 <95719434+jlnk03@users.noreply.github.com>
2023-05-08 23:40:21 +0200

    git ignore and assets

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # autoAI
 
 Chat Interface to use with an LLM. Includes a normal interaction as well as the possibility to use agents. Agents are able to access the internet, wikipedia or use the Python REPL to execute code.
+
+### Image of the chat interface using agents:
+
+![chat_agent](assets/chat_agent.png)
+
+
+### Image of the chat interface and code format:
+Code will be highlighted and can be copied to the clipboard by pressing the button in the right upper corner.
+
+![chat_code](assets/chat_code.png)
+
+### Setup
+To use the app an api key from OpenAI is needed. Set the environment variable OPENAI_API_KEY.
+If you want to use specific formatting for the code, download the files form the [highlight.js](https://highlightjs.org/download/) website and put them in the assets folder or sub-folder.
+
+### Run
+To run the app execute the following command:
+```
+python3 app.py
+```
+or in a python environment:
+```
+app.py
+```
---
+++ assets/code_format.js
@@ -0,0 +1,38 @@
+window.dash_clientside = Object.assign({}, window.dash_clientside, {
+    clientside: {
+        codeStyle: function () {
+            let codeBlocks = document.querySelectorAll('pre code');
+            for (let i = 0; i < codeBlocks.length; i++) {
+                // hljs.highlightBlock(codeBlocks[i]);
+                codeBlocks[i].classList.add('rounded-lg', 'my-4', 'bg-slate-700', 'relative');
+
+                const copyButton = document.createElement('img');
+                copyButton.className = 'h-6 absolute top-0 right-0 m-2 text-slate-300 text-sm hover:text-slate-100 bg-slate-600 hover:bg-slate-500 rounded-md cursor-pointer p-1';
+                // copyButton.textContent = 'Copy';
+                copyButton.src = 'assets/copy_icon.png';
+
+                console.log('copyButton')
+                console.log(copyButton)
+
+                copyButton.addEventListener('click', function() {
+                    const codeContent = codeBlocks[i].textContent;
+                    navigator.clipboard.writeText(codeContent).then(function() {
+                        alert('Code copied to clipboard!');
+                    });
+                });
+
+                codeBlocks[i].appendChild(copyButton);
+                // codeBlocks[i].insertBefore(copyButton, codeBlocks[i]);
+                console.log('this is codeBlocks[i]')
+                console.log(codeBlocks[i]);
+
+            }
+
+            let inlineCodeBlocks = document.querySelectorAll('code');
+            for (let i = 0; i < inlineCodeBlocks.length; i++) {
+                inlineCodeBlocks[i].classList.add('font-bold');
+            }
+
+        }
+    }
+});
